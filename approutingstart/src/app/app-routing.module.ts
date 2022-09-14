25bcb8ef59b7564cdc972ec540160ff438f4f91e
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-09-14 08:05:23 +0000

    dopo primo set di domande

--- approutingstart/src/app/app-routing.module.ts
+++ approutingstart/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalComponent } from './animal/animal.component';
+import { FruitsComponent } from './fruits/fruits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'animals', component: AnimalComponent},
+  { path: 'fruits', component: FruitsComponent}]; //<- Qui vanno inserite tutte le route che desideriamo creare
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- approutingstart/src/app/app.module.ts
+++ approutingstart/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnimalsComponentComponent } from './animals-component/animals-component.component';
-import { FruitsComponentComponent } from './fruits-component/fruits-component.component';
+
+import { FruitsComponent } from './fruits/fruits.component';
+import { AnimalComponent } from './animal/animal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnimalsComponentComponent,
-    FruitsComponentComponent
+    FruitsComponent,
+    AnimalComponent
   ],
   imports: [
     BrowserModule,
