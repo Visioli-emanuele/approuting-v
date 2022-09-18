00c0c574ee2cb56a01ee04df70a60f8f82986bf1
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-09-18 15:31:55 +0000

    fine dei lavori

--- approutingstart/src/app/app-routing.module.ts
+++ approutingstart/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalComponent } from './animal/animal.component';
 import { FruitsComponent } from './fruits/fruits.component';
+import { GenericComponent } from './generic/generic.component';
+
 
 const routes: Routes = [
   { path: 'animals', component: AnimalComponent},
   { path: 'fruits', component: FruitsComponent}, //<- Qui vanno inserite tutte le route che desideriamo creare
-  {path: '', redirectTo: '/animals', pathMatch: 'full'}];
+  {path: '', redirectTo: '/animals', pathMatch: 'full'},
+  { path: 'generic/:id', component: GenericComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
